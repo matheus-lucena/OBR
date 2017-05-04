2584698e44e43e755ffd4e262f01d191ddf53fc7
matheus-gio <matheus.giovanni.lu@gmail.com>
2017-05-04 14:59:24 -0300

    Add files via upload

---
+++ Seguir_Linha.ino
@@ -0,0 +1,42 @@
+#include <OBR.h>
+
+Controle Motor(3,4,5,6,7,8); // in1 , in2 , pwma, in3, in4, pmwb
+
+Sensores sensor1; // Define tempo leitura sensores 
+Sensores sensor2;
+
+int LinhaDireita, LinhaEsquerda;
+
+int time_refletance = 500;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor1.Leitura(9,0,1,1,time_refletance); // Porta // Porta Ligação Sensor SOMENTE PORTA DIGITAL 0 - Desativa / 1-Ativa // 0 - Analogico / 1 - Digital // 0 - desativa Millis / 1 - Ativa Millis  // Tempo millis - NECESSIDADE DE AJUSTAS
+  sensor2.Leitura(10,0,1,1,time_refletance);
+  
+  LinhaEsquerda = sensor1.value;
+  LinhaDireita = sensor2.value;
+
+
+  if( LinhaEsquerda == 0 && LinhaDireita == 0){
+  Motor.pwm(200,200);  
+  }
+
+  if( LinhaEsquerda == 0 && LinhaDireita == 1){
+  Motor.pwm(200,0);  
+  }
+
+  if( LinhaEsquerda == 1 && LinhaDireita == 0){
+  Motor.pwm(0,200);  
+  }
+
+  if( LinhaEsquerda == 1 && LinhaDireita == 1){
+  Motor.pwm(200,200);  
+  }
+  
+  }
